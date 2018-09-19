3e36a89d7a544303c6f76da9d099e5dc4cb0eb12
phinome <Long.phinome@gmail.com>
2018-09-19 14:48:08 +0800

    infoList component developing

--- src/components/InfoList.js
+++ src/components/InfoList.js
@@ -1,79 +1,83 @@
 import React from "react";
 import { List, Icon, Avatar } from "antd";
-import { Link } from "react-router-dom";
+import AutoSizer from "react-virtualized/dist/commonjs/AutoSizer";
+import VList from "react-virtualized/dist/commonjs/List";
+import { get } from "../utils";
 
 import "./infoList.styles.less";
 
-const data = [
-  {
-    title: "Ant Design Title 1"
-  },
-  {
-    title: "Ant Design Title 2"
-  },
-  {
-    title: "Ant Design Title 3"
-  },
-  {
-    title: "Ant Design Title 4"
-  },
-  {
-    title: "Ant Design Title 1"
-  },
-  {
-    title: "Ant Design Title 2"
-  },
-  {
-    title: "Ant Design Title 3"
-  },
-  {
-    title: "Ant Design Title 4"
-  },
-  {
-    title: "Ant Design Title 1"
-  },
-  {
-    title: "Ant Design Title 2"
-  },
-  {
-    title: "Ant Design Title 3"
-  },
-  {
-    title: "Ant Design Title 4"
+export default class InfoList extends React.PureComponent {
+  state = {
+    data: []
+  };
+
+  async componentDidMount() {
+    const data = await get();
+    this.setState({
+      data
+    });
   }
-];
 
-const InfoListItem = item => (
-  <List.Item>
-    <List.Item.Meta
-      avatar={
-        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-      }
-      title={<a href="https://ant.design">{item.title}</a>}
-      description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-    />
-  </List.Item>
-);
+  renderItem({ index }) {
+    const { data } = this.state;
+    const item = data[index];
+    return (
+      <List.Item key={item.blockId}>
+        <List.Item.Meta
+          avatar={
+            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          }
+          title={<a href="https://ant.design">{item.title}</a>}
+          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+        />
+      </List.Item>
+    );
+  }
 
-const InfoList = props => {
-  const { iconType, title, href, ...listProps } = props;
-  return (
-    <div className="infoList">
-      <div className="infoList-panel-heading">
-        <h2 className="infoList-panel-title">
-          <Icon type={iconType} theme="outlined" />
-          {title}
-        </h2>
-        {/* <Link to={href}>View All</Link> */}
-      </div>
-      <List
-        className="list-container"
-        dataSource={data}
-        itemLayout="horizontal"
-        renderItem={InfoListItem}
+  render() {
+    const { data } = this.state;
+    const vlist = ({
+      height,
+      isScrolling,
+      onChildScroll,
+      scrollTop,
+      onRowsRendered,
+      width
+    }) => (
+      <VList
+        autoHeight
+        height={height}
+        isScrolling={isScrolling}
+        onScroll={onChildScroll}
+        overscanRowCount={2}
+        rowCount={data.length}
+        rowHeight={77}
+        rowRenderer={this.renderItem}
+        onRowsRendered={onRowsRendered}
+        scrollTop={scrollTop}
+        width={width}
       />
-    </div>
-  );
-};
-
-export default InfoList;
+    );
+    const autoSize = ({
+      height,
+      isScrolling,
+      onChildScroll,
+      scrollTop,
+      onRowsRendered
+    }) => (
+      <AutoSizer disableHeight>
+        {({ width }) =>
+          vlist({
+            height,
+            isScrolling,
+            onChildScroll,
+            scrollTop,
+            onRowsRendered,
+            width
+          })
+        }
+      </AutoSizer>
+    );
+    return <List>{data.length > 0 && <WindowScroller />}</List>;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import "./index.less";
-
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { LocaleProvider } from "antd";
 import zh_CN from "antd/lib/locale-provider/zh_CN";
+
+import "react-virtualized/styles.css";
+import "./index.less";
 
 import App from "./App";
 
