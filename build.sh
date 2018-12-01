d4f0dd6c89827ae244b71c7a74aa2056fab95dc5
hzz780 <hzz780@qq.com>
2018-12-01 19:18:22 +0800

    update shell

--- build.sh
+++ build.sh
@@ -1,22 +1,42 @@
-#!/bin/sh
+#!/bin/bash
+
+cat << EOT
+            _    _____ _  __       _____            _
+           / \  | ____| |/ _|     | ____|_  ___ __ | | ___  _ __ ___ _ __
+          / _ \ |  _| | | |_ _____|  _| \ \/ / '_ \| |/ _ \| '__/ _ \ '__|
+         / ___ \| |___| |  _|_____| |___ >  <| |_) | | (_) | | |  __/ |
+        /_/   \_\_____|_|_|       |_____/_/\_\ .__/|_|\___/|_|  \___|_|
+                                             |_|
+
+EOT
 
 NGINX_CONF="/etc/nginx/conf.d"
 ROOT_DIR="/opt/aelf/app/explorer"
 
+#当变量a为null或为空字符串时则var=b
+start_mode=${1:-'production'}
+node_modules_action=${2:-'default'}
+echo ${node_modules_action} ${start_mode}
+
 git pull origin master
 
-if [ $2 == 'reinstall' ]
+if [ ${node_modules_action} = 'reinstall' ]
 then
-    echo 'rm -rf node_modules';
-    rm -rf node_modules;
-    echo 'npm install';
-    npm install;
+    echo 'npm install'
+    npm install && echo 'install done'
+    sleep 3
 fi
-#npm i && npm run build
-npm run build
 
-cp -rf dist/* $ROOT_DIR
+if [ ${start_mode} = 'dev' ]
+then
+    npm start
+    echo 'npm start'
+else
+    #npm i && npm run build
+    npm run build
+    cp -rf dist/* $ROOT_DIR
+    cp -f explore.conf $NGINX_CONF
+    nginx -s reload
+    echo 'npm run build && nginx -s reload'
+fi
 
-cp -f explore.conf $NGINX_CONF
-
-nginx -s reload
