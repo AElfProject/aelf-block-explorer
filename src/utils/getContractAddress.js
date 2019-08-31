9d85c69b90e5c8c2c4fda96be3d209754a0bbb74
Young <857531530@qq.com>
2019-08-31 21:15:49 +0800

    chore: Add some suitable name

--- src/utils/getContractAddress.js
+++ src/utils/getContractAddress.js
@@ -2,24 +2,41 @@
  * @file getContractAddress.js
  * @author zhouminghui
  * 获取查询使用的公共钱包与合约地址
-*/
+ */
 
-import {aelf} from '../utils';
+import { aelf } from '../utils';
 import * as AElf from 'aelf-sdk';
-import {commonPrivateKey, multiToken, consensusDPoS, dividends, tokenConverter} from '../../config/config';
+// todo: The exist of dividends is for compatibility, after all man use the more accurate name 'dividendContractAddr', we can drop the name of 'dividends'.
+import {
+  commonPrivateKey,
+  multiToken,
+  consensusDPoS,
+  dividends,
+  tokenConverter,
+  dividendContractAddr,
+  electionContractAddr,
+  voteContractAddr,
+  consensusContractAddr,
+  multiTokenContractAddr
+} from '@config/config';
 export default function getContractAddress() {
-    return new Promise((resolve, reject) => {
-        const wallet = AElf.wallet.getWalletByPrivateKey(commonPrivateKey);
-        aelf.chain.getChainStatus((error, result) => {
-            const output = {
-                consensusDPoS,
-                dividends,
-                multiToken,
-                tokenConverter,
-                wallet,
-                chainInfo: result
-            };
-            resolve(output);
-        });
+  return new Promise((resolve, reject) => {
+    const wallet = AElf.wallet.getWalletByPrivateKey(commonPrivateKey);
+    aelf.chain.getChainStatus((error, result) => {
+      const output = {
+        consensusDPoS,
+        dividends,
+        multiToken,
+        tokenConverter,
+        wallet,
+        chainInfo: result,
+        dividendContractAddr,
+        voteContractAddr,
+        electionContractAddr,
+        consensusContractAddr,
+        multiTokenContractAddr
+      };
+      resolve(output);
     });
+  });
 }
