e119b8c1a3b652ea0ca53fd6175d6101cf6894e7
Alfred Yang <alfred.yang@aelf.io>
2019-12-07 14:52:58 +0800

    style: 💄 modify the order of two components, patch the rulecard

--- src/pages/Vote/ElectionNotification/ElectionRuleCard/ElectionRuleCard.js
+++ src/pages/Vote/ElectionNotification/ElectionRuleCard/ElectionRuleCard.js
@@ -7,23 +7,25 @@ import './ElectionRuleCard.style.less';
 export default memo(function ElectionRuleCard(props) {
   const { isCandidate, currentWallet } = props;
   return (
-    <section className='election-rule-card'>
-      <h2 className='election-header-title'>Node Election</h2>
-      <div className='election-container'>
-        <p className='election-intro'>
-          Every token holder has the opportunity to become a BP node. However, in
-          order to make our networks and communities operate more smoothly and
-          effectively, we have developed a set of standards and regulations to
-          make eligible people candidate nodes. We increased their chances of
-          being elected by voting. We will vote on the new BP consensus node every
-          week and publish the election results.
+    <section className="election-rule-card">
+      <h2 className="election-header-title">Node Election</h2>
+      <div className="election-container">
+        <p className="election-intro">
+          Every token holder has the opportunity to become a BP node. However,
+          in order to make our networks and communities operate more smoothly
+          and effectively, we have developed a set of standards and regulations
+          to make eligible people candidate nodes. We increased their chances of
+          being elected by voting. We will vote on the new BP consensus node
+          every week and publish the election results.
+          <a className="view-plan-link" href="">
+            View the node election plan >
+          </a>
         </p>
-        <p><a href=''>View the node election plan ></a></p>
-        <div className='btn-group'>
+        <div className="btn-group">
           {/*<button className='view-node-election-plan-btn'>*/}
           {/*<a href=''>View the node election plan ></a>*/}
           {/*</button>*/}
-          <Button type='primary' className='apply-to-be-a-node-btn'>
+          <Button type="primary" className="apply-to-be-a-node-btn">
             {isCandidate ? (
               <Link
                 to={{
@@ -34,7 +36,7 @@ export default memo(function ElectionRuleCard(props) {
                 Modify team information
               </Link>
             ) : (
-              <Link to='/vote/apply'>Become a candidate node</Link>
+              <Link to="/vote/apply">Become a candidate node</Link>
             )}
           </Button>
         </div>
