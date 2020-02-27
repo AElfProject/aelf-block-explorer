a7c476ef86da418947e272d07cda03bb21632975
atom-young <atom.yang@aelf.io>
2020-02-27 13:07:51 +0800

    add ifram url reload

--- src/pages/Viewer/index.jsx
+++ src/pages/Viewer/index.jsx
@@ -3,85 +3,48 @@
  * @author atom-yang
  */
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+import useLocation from 'react-use/lib/useLocation';
+import IFrame from '../../components/IFrame';
 import './index.less';
 
-async function innerHeight(time = 0, timeout = 500, maxTime = 10) {
-    const currentTime = time + 1;
-    if (currentTime > maxTime) {
-        return '100vh';
-    }
-    try {
-        const height =
-            document.querySelector('#viewer').contentWindow.querySelector('#app').clientHeight;
-        if (height && height > 400) {
-            return `${height + 100}px`;
-        }
-        throw new Error('invalid');
-    } catch (e) {
-        await new Promise(resolve => {
-           setTimeout(() => {
-               resolve();
-           }, timeout);
-        });
-        return innerHeight(currentTime);
-    }
-}
 
+// const DEFAULT_URL = process.env.NODE_ENV === 'production' ? '/viewer/list.html' : 'http://0.0.0.0:8526/list.html';
 
+const DEFAULT_URL = '/viewer/list.html';
 
 const Viewer = props => {
-    const { url } = props;
+
+    const location = useLocation();
+    const [url, setUrl] = useState(DEFAULT_URL);
+
+    function onChange(href) {
+        console.log(href);
+        window.history.replaceState(
+            window.history.state,
+            '',
+            `${location.origin}${location.pathname}?#${encodeURIComponent(href)}`);
+    }
 
     useEffect(() => {
-        function listener(event) {
-            const {
-                data,
-                origin
-            } = event;
-            console.log(origin);
-            if (
-                (origin === 'http://0.0.0.0:8526' && process.env.NODE_ENV !== 'production')
-                || (origin === location.origin && process.env.NODE_ENV === 'production')) {
-                console.log(data);
-                const {
-                    height
-                } = data;
-                if (height) {
-                    document.querySelector('#viewer').style.height = height;
-                }
-            }
+        const {
+            hash
+        } = location;
+        if (hash) {
+            console.log(decodeURIComponent(hash));
+            setUrl(decodeURIComponent(hash).split('#')[1]);
         }
-        window.addEventListener('message', listener);
-        return () => {
-            window.removeEventListener('message', listener);
-        };
     }, []);
 
     return (
         <div
             className="explorer-viewer-iframe"
         >
-            <iframe
-                id="viewer"
-                name="viewer"
-                src={url}
-                frameBorder="0"
-                marginHeight="0"
-                marginWidth="0"
-                align="middle"
-                scrolling="auto"
+            <IFrame
+                url={url}
+                onChange={onChange}
             />
         </div>
     );
 };
 
-Viewer.propTypes = {
-    url: PropTypes.string
-};
-
-Viewer.defaultProps = {
-    url: process.env.NODE_ENV === 'production' ? '/viewer/list.html' : 'http://0.0.0.0:8526/list.html'
-};
-
-module.exports = Viewer;
+export default Viewer;
