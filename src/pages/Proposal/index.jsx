f2b7e2f7db7cf32c2cb94d77192dc397c79da85b
atom-young <atom.yang@aelf.io>
2020-02-27 14:29:55 +0800

    add router

---
+++ src/pages/Proposal/index.jsx
@@ -0,0 +1,48 @@
+/**
+ * @file contract viewer
+ * @author atom-yang
+ */
+import React, { useEffect, useState } from 'react';
+import useLocation from 'react-use/lib/useLocation';
+import IFrame from '../../components/IFrame';
+
+
+// const DEFAULT_URL = process.env.NODE_ENV === 'production' ? '/viewer/list.html' : 'http://0.0.0.0:8526/list.html';
+
+const DEFAULT_URL = '/viewer/proposal.html';
+
+const Proposal = () => {
+
+    const location = useLocation();
+    const [url, setUrl] = useState(DEFAULT_URL);
+
+    function onChange(href) {
+        console.log(href);
+        window.history.replaceState(
+            window.history.state,
+            '',
+            `${location.origin}${location.pathname}?#${encodeURIComponent(href)}`);
+    }
+
+    useEffect(() => {
+        const {
+            hash
+        } = location;
+        if (hash) {
+            setUrl(decodeURIComponent(hash).split('#')[1]);
+        }
+    }, []);
+
+    return (
+        <div
+            className="explorer-viewer-iframe"
+        >
+            <IFrame
+                url={url}
+                onChange={onChange}
+            />
+        </div>
+    );
+};
+
+export default Proposal;
