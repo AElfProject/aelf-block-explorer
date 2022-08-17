8ef8de953709af5dfb4c273527c29f8ea6886925
AbigailDeng <Abigail.deng@ienyan.com>
2022-08-17 10:40:17 +0800

    feat: webpack prod

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@
  */
 /* eslint-disable fecs-camelcase */
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom";
 import { Provider } from "react-redux";
 import Cookies from "js-cookie";
 
