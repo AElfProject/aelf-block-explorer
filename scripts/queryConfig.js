0dd02b77bcb6bd66311412b305adf8fdb69234c5
AbigailDeng <Abigail.deng@ienyan.com>
2022-08-18 15:33:43 +0800

    feat: fix queryConfig

--- scripts/queryConfig.js
+++ scripts/queryConfig.js
@@ -1,125 +1,109 @@
-/* eslint-disable global-require */
-const path = require("path");
-const fs = require("fs");
+// polyfill
+global.location = {};
 const AElf = require("aelf-sdk");
-const config = require("../config");
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");
+const config = require("../config/config.json");
+const mergedConfig = require("../config/config.js");
+const axios = require("axios");
 
-const { host } = config.scan;
+dotenv.config("../.env");
 
-const contractNames = [
-  {
-    name: "Token",
-    description: "contract Token",
-    contractName: "AElf.ContractNames.Token",
-  },
-  {
-    name: "Dividend",
-    description: "contract Dividend",
-    contractName: "AElf.ContractNames.Treasury",
-  },
-  {
-    name: "Consensus.Dpos",
-    description: "contract Consensus",
-    contractName: "AElf.ContractNames.Consensus",
-  },
-  {
-    name: "Token Converter",
-    description: "contract Token Converter",
-    contractName: "AElf.ContractNames.TokenConverter",
-  },
-  {
-    name: "Election",
-    description: "contract Election",
-    contractName: "AElf.ContractNames.Election",
-  },
-  {
-    name: "Profit",
-    description: "contract Profit",
-    contractName: "AElf.ContractNames.Profit",
-  },
-  {
-    name: "Parliament",
-    description: "contract Parliament",
-    contractName: "AElf.ContractNames.Parliament",
-  },
-  {
-    name: "Association",
-    description: "contract Association",
-    contractName: "AElf.ContractNames.Association",
-  },
-  {
-    name: "Referendum",
-    description: "contract Referendum",
-    contractName: "AElf.ContractNames.Referendum",
-  },
-  {
-    name: "CrossChain",
-    description: "contract CrossChain",
-    contractName: "AElf.ContractNames.CrossChain",
-  },
-];
+const aelf = new AElf(
+  new AElf.providers.HttpProvider(process.env.CHAIN_ENDPOINT)
+);
+const wallet = AElf.wallet.getWalletByPrivateKey(mergedConfig.commonPrivateKey);
 
-async function getContractAddress() {
-  const aelf = new AElf(new AElf.providers.HttpProvider(host));
-  const wallet = AElf.wallet.createNewWallet();
+const result = {
+  ...config,
+};
+
+async function getConfig() {
+  const { CONTRACTS, schemeIds } = config;
   const { ChainId, GenesisContractAddress } = await aelf.chain.getChainStatus();
-  let result = {
-    chainId: ChainId,
-  };
+  result.CHAIN_ID = ChainId;
   const zeroContract = await aelf.chain.contractAt(
     GenesisContractAddress,
     wallet
   );
-  const list = await Promise.all(
-    contractNames.map(async (item) => {
-      const { contractName, name, ...left } = item;
-      try {
-        const address = await zeroContract.GetContractAddressByName.call(
-          AElf.utils.sha256(contractName)
-        );
-        return {
-          ...left,
-          contractAddress: address,
-          contractName: name,
-        };
-      } catch (e) {
-        return {
-          ...left,
-          contractAddress: "",
-          contractName: name,
-        };
-      }
-    })
-  ).then((results) => results.filter((item) => item.contractAddress));
-  console.log(list);
-  result = {
-    ...result,
-    contractAddress: [
-      {
-        contractName: "Genesis",
-        description: "contract Genesis",
-        contractAddress: GenesisContractAddress,
-      },
-      ...list,
-    ],
-  };
-  const originResult = JSON.parse(
-    fs.readFileSync(path.resolve(__dirname, "../config.json")).toString()
-  );
-  result = {
-    ...originResult,
-    viewer: {
-      ...originResult.viewer,
-      ...result,
-    },
-  };
+  for (const [key, name] of Object.entries(CONTRACTS)) {
+    const contractAddress = await zeroContract.GetContractAddressByName.call(
+      AElf.utils.sha256(name)
+    );
+    result[key] = contractAddress;
+    if (name === "AElf.ContractNames.Token") {
+      const contract = await aelf.chain.contractAt(contractAddress, wallet);
+      const { symbol } = await contract.GetNativeTokenInfo.call();
+      let resourceTokens = await contract.GetResourceTokenInfo.call();
+      resourceTokens = Array.isArray(resourceTokens.value)
+        ? resourceTokens.value.map(({ symbol, decimals }) => ({
+            symbol,
+            decimals,
+          }))
+        : [];
+      result.SYMBOL = symbol;
+      result.resourceTokens = resourceTokens;
+    }
+  }
+  if (result.electionContractAddr) {
+    const profit = await aelf.chain.contractAt(
+      result.profitContractAddr,
+      wallet
+    );
+    const electionSchemeIds = (
+      await profit.GetManagingSchemeIds.call({
+        manager: result.electionContractAddr,
+      })
+    ).schemeIds;
+    const treasurySchemaIds = (
+      await profit.GetManagingSchemeIds.call({
+        manager: result.dividends,
+      })
+    ).schemeIds;
+    const schemes = [
+      electionSchemeIds[1],
+      electionSchemeIds[0],
+      treasurySchemaIds[3],
+      treasurySchemaIds[2],
+      treasurySchemaIds[4],
+    ].map((v, i) => ({
+      type: schemeIds[i].type,
+      schemeId: v,
+    }));
+    result.schemeIds = schemes;
+  }
+  result.genesisContract = GenesisContractAddress;
   console.log(result);
   fs.writeFileSync(
-    path.resolve(__dirname, "../config.json"),
+    path.resolve("./config/config.json"),
     `${JSON.stringify(result, null, 2)}\n`
   );
 }
 
-(async () => {
-  await getContractAddress();
-})();
+// enum netWorkType: MAIN | TESTNET
+// get cms data
+async function getCMS() {
+  try {
+    const res = await axios({
+      method: "get",
+      url: "https://test-cms.aelf.io/cms/chain-list-by-networks",
+      params: {
+        // populate: "chain",
+        // "filters[netWorkType][$eq]": mergedConfig.NETWORK_TYPE,
+        netWorkType: mergedConfig.NETWORK_TYPE,
+      },
+    });
+    const data = res?.data?.[0] ?? {};
+    fs.writeFileSync(
+      path.resolve("./config/configCMS.json"),
+      `${JSON.stringify(data, null, 2)}\n`
+    );
+  } catch (error) {
+    throw error;
+  }
+}
+
+getCMS().catch(console.error);
+
+getConfig().catch(console.error);
