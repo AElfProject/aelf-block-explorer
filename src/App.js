c5c2cf544c4b4c8cbadda71d62dcd7b8de20e985
minghao-98 <93118284+minghao-98@users.noreply.github.com>
2022-09-13 17:26:56 +0800

    feat: 🎸 back to top

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @file App
  * @author huangzongzhe
  */
-import React, { Suspense } from "react";
+import React, { Suspense, useCallback, useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import BrowserHeader from "./components/Header/Header";
 import HeaderBlank from "./components/Header/HeaderBlank";
@@ -10,10 +10,24 @@ import BrowserFooter from "./components/Footer/Footer";
 import BrowserBreadcrumb from "./components/Breadcrumb/Breadcrumb";
 import Container from "./components/Container/Container";
 import { PageRouter } from "./routes/routes";
+import { useLocation } from "react-use";
 
 import "./App.less";
 
 function App() {
+  const { pathname } = useLocation()
+
+  const back2Top = useCallback(() => {
+    const app = document.querySelector('#app')
+    if (app) {
+      app.scrollIntoView({ block: 'start', behavior: 'auto' })
+    }
+  }, [])
+
+  useEffect(() => {
+    back2Top()
+  }, [pathname])
+
   return (
     <Suspense fallback={null}>
       <div className='App'>
