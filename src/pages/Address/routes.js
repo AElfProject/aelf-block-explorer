bdf5e3a657e4174c1e263007eb9c7f53fef9ee13
minghao-98 <93118284+minghao-98@users.noreply.github.com>
2022-09-16 15:47:11 +0800

    refactor: 💡 remove files

--- src/pages/Address/routes.js
+++ src/pages/Address/routes.js
@@ -1,18 +1,9 @@
-import { lazy } from "react";
-const ContractList = lazy(() => import("./containers/ContractList"));
-const AccountList = lazy(() => import("./containers/AccountList"));
-const AccountInfo = lazy(() => import("./containers/AccountInfo"));
+/* eslint-disable import/prefer-default-export */
+import React, { lazy } from "react";
+
 const TokenList = lazy(() => import("./containers/TokenList"));
 const TokenInfo = lazy(() => import("./containers/TokenInfo"));
 export const AddressRouter = [
-  // {
-  //   // We cannot use ? in v6
-  //   path: "/address/:address/:symbol",
-  //   element: <AccountInfo />,
-  // },
-  // { path: "/contract", element: <ContractList /> },
-  // { path: "/contract/:address", element: <ContractInfo /> },
-  // { path: "/contract/:address/:codeHash", element: <ContractInfo /> },
   { path: "/token", element: <TokenList /> },
   { path: "/token/:symbol", element: <TokenInfo /> },
 ];
