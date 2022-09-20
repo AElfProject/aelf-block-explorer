dbf333e021601c644ff8b98af948d5bf9393ddd1
minghao-98 <93118284+minghao-98@users.noreply.github.com>
2022-09-20 11:40:10 +0800

    fix: 🐛 menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -120,7 +120,8 @@ class BrowserHeader extends PureComponent {
 
       if (target && current !== target[1]) {
         // white list
-        [pathname] = target;
+        [, pathname] = target;
+        console.log('first')
         this.setState({
           current: pathname,
           showSearch,
