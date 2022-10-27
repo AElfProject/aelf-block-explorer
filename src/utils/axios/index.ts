bc3f49a7a21527f3766c195df99352527991761d
AbigailDeng <Abigail.deng@ienyan.com>
2022-10-27 13:50:32 +0800

    feat: error handle

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -72,7 +72,7 @@ const getSSR = async (ctx: NextPageContext, url: string, params?: any, config?: 
   const baseUrl = '/api';
   const wholeUrl = config?.onlyUrl ? url : `${host}${baseUrl}${url}`;
   const res = await api.get(wholeUrl, params, config);
-  // console.log(res, 'res');
+  console.log(res, 'res');
   if (res.ok) {
     return res.data as any;
   } else {
