68bdbf7b65a83eaa8202d14544bfa505059c27b9
AbigailDeng <Abigail.deng@ienyan.com>
2022-11-09 16:26:35 +0800

    fix: old url

--- src/utils/guard.js
+++ src/utils/guard.js
@@ -1,17 +1,21 @@
 //global routes guard
 export function guard(location, navigate) {
   const { pathname } = location;
+  const hash = location.hash;
+  let url = "";
   //Compatible with old url
-  if (pathname.indexOf("viewer") > -1) {
-    const hash = location.hash;
-    const route = hash.split("/")[1];
-    let url = "";
-    if (pathname.indexOf("proposal") > -1) {
+  if (pathname.indexOf("proposal") > -1 && hash) {
+    const decodeHash = decodeURIComponent(hash).substring(1);
+    if (decodeHash.indexOf("viewer") > -1) {
+      const route = decodeHash.split("#/")[1];
       url = `/proposal/${route}`;
-    } else {
-      const item = hash.split("/")[2];
-      url = `/${route}/${item}`;
     }
+    navigate(url);
+    return false;
+  } else if (pathname.indexOf("viewer") > -1) {
+    const route = hash.split("/")[1];
+    const item = hash.split("/")[2];
+    url = item ? `/${route}/${item}` : `/${route}`;
     navigate(url);
     return false;
   }
