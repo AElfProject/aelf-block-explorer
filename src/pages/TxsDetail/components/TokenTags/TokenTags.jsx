c767d235f9092e669a5eb5058e4c2a83ebcbbc55
AbigailDeng <Abigail.deng@ienyan.com>
2023-03-15 10:17:29 +0800

    feat: decimal null

--- src/pages/TxsDetail/components/TokenTags/TokenTags.jsx
+++ src/pages/TxsDetail/components/TokenTags/TokenTags.jsx
@@ -1,12 +1,11 @@
 import { Button, Spin, Tag } from "antd";
-import React, { useEffect, useMemo, useState } from "react";
-
+import React, { useEffect, useMemo, useState, useCallback } from "react";
+import { useEffectOnce } from "react-use";
 import "./TokenTags.style.less";
 import IconFont from "../../../../components/IconFont";
-import { useCallback } from "react";
 import { get } from "../../../../utils";
-import { useEffectOnce } from "react-use";
 import { VIEWER_GET_ALL_TOKENS } from "../../../../constants";
+
 export default function TokenTag({ values, isDone, price }) {
   const [showMore, setShowMore] = useState(false);
   const [hasMore, setHasMore] = useState(false);
@@ -61,17 +60,21 @@ export default function TokenTag({ values, isDone, price }) {
           {isDone || forceDone ? (
             elfFirst.map((key) => {
               const decimal = decimals[key] || 0;
-              const val = values[key] / Math.pow(10, decimal);
+              const val = values[key] / 10 ** decimal;
               return (
                 <div key={key}>
-                  <Tag>
-                    {`${val.toLocaleString(undefined, {
-                      maximumFractionDigits: decimal,
-                    })} ${key}`}
-                    {key === "ELF" && (
-                      <span>(${(price.USD * val).toFixed(2)})</span>
-                    )}
-                  </Tag>
+                  {Object.keys(decimals).length ? (
+                    <Tag>
+                      {`${val.toLocaleString(undefined, {
+                        maximumFractionDigits: decimal,
+                      })} ${key}`}
+                      {key === "ELF" && (
+                        <span>(${(price.USD * val).toFixed(2)})</span>
+                      )}
+                    </Tag>
+                  ) : (
+                    "-"
+                  )}
                 </div>
               );
             })
