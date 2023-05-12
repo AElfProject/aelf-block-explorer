08251deb2d7788b7a95bf69a4eb444931dae2b44
holymingo <holymingo@aelf.io>
2023-05-12 17:17:00 +0800

    refactor: 💡 fill code in process functions

--- src/pages/Vote/MyVote/MyVote.js
+++ src/pages/Vote/MyVote/MyVote.js
@@ -1,22 +1,19 @@
-import React, { useState, useEffect } from "react";
-import moment from "moment";
-import StatisticalData from "@components/StatisticalData/";
-import { getAllTeamDesc, fetchPageableCandidateInformation } from "@api/vote";
-import publicKeyToAddress from "@utils/publicKeyToAddress";
-import {
-  RANK_NOT_EXISTED_SYMBOL,
-  ELF_DECIMAL,
-  myVoteStatistData,
-} from "@src/pages/Vote/constants";
-import { MY_VOTE_DATA_TIP } from "@src/constants";
-import { Button, Spin } from "antd";
-import NightElfCheck from "../../../utils/NightElfCheck";
-import getLogin from "../../../utils/getLogin";
-import MyVoteRecord from "./MyVoteRecords";
-import { getPublicKeyFromObject } from "../../../utils/getPublicKey";
-import addressFormat from "../../../utils/addressFormat";
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 
-import "./MyVote.style.less";
+import StatisticalData from '@components/StatisticalData/';
+import { getAllTeamDesc, fetchPageableCandidateInformation } from '@api/vote';
+import publicKeyToAddress from '@utils/publicKeyToAddress';
+import { RANK_NOT_EXISTED_SYMBOL , ELF_DECIMAL, myVoteStatistData } from '@src/pages/Vote/constants';
+import { MY_VOTE_DATA_TIP } from '@src/constants';
+import { Button, Spin } from 'antd';
+import NightElfCheck from '../../../utils/NightElfCheck';
+import getLogin from '../../../utils/getLogin';
+import MyVoteRecord from './MyVoteRecords';
+import { getPublicKeyFromObject } from '../../../utils/getPublicKey';
+import addressFormat from '../../../utils/addressFormat';
+
+import './MyVote.style.less';
 
 const MyVote = (props) => {
   const [statistData, setStatistData] = useState(myVoteStatistData);
@@ -30,7 +27,6 @@ const MyVote = (props) => {
       y: null,
     },
   });
-
   const [hasRun, setHasRun] = useState(false);
 
   useEffect(() => {
@@ -40,38 +36,38 @@ const MyVote = (props) => {
   }, [props.currentWallet]);
 
   useEffect(() => {
-    if (props.currentWallet && !hasRun) {
+    if (!hasRun) {
       fetchTableDataAndStatistData();
-      setHasRun(true);
     }
-  }, [props.currentWallet, hasRun]);
+  }, [hasRun]);
 
+  
   const getCurrentWallet = () => {
     NightElfCheck.getInstance()
       .check.then((ready) => {
         const nightElf = NightElfCheck.getAelfInstanceByExtension();
         getLogin(
           nightElf,
-          { file: "MyVote.js" },
+          { file: 'MyVote.js' },
           (result) => {
             if (result.error) {
               setSpinningLoading(false);
             } else {
               const wallet = JSON.parse(result.detail);
-              const newCurrentWallet = {
+              const currentWallet = {
                 formattedAddress: addressFormat(wallet.address),
                 address: wallet.address,
                 name: wallet.name,
                 pubKey: getPublicKeyFromObject(wallet.publicKey),
               };
-              setCurrentWallet(newCurrentWallet);
+              setCurrentWallet(currentWallet);
               props.checkExtensionLockStatus();
               setTimeout(() => {
-                fetchTableDataAndStatistData(newCurrentWallet);
+                fetchTableDataAndStatistData(currentWallet);
               });
             }
           },
-          false
+          false,
         );
       })
       .catch((error) => {
@@ -83,19 +79,22 @@ const MyVote = (props) => {
     const { electionContract } = props;
     if (!electionContract) return;
     setHasRun(true);
-    const newCurrentWallet = currentWalletTemp || currentWallet;
-    if (!newCurrentWallet || !newCurrentWallet.address) {
+    const currentWallet = currentWalletTemp || currentWallet;
+    if (!currentWallet || !currentWallet.address) {
       setHasRun(false);
       return false;
     }
 
+    // todo: is it ok to get the same data twice in different tabs
+    // todo: add error handle
     Promise.all([
       electionContract.GetElectorVoteWithAllRecords.call({
-        value: newCurrentWallet.pubKey,
+        value: currentWallet.pubKey,
       }),
       getAllTeamDesc(),
       fetchPageableCandidateInformation(electionContract, {
         start: 0,
+        // length: A_NUMBER_LARGE_ENOUGH_TO_GET_ALL // FIXME:
         length: 20,
       }),
     ])
@@ -103,28 +102,127 @@ const MyVote = (props) => {
         processData(resArr);
       })
       .catch((err) => {
-        console.error("err", "fetchTableDataAndStatistData", err);
+        console.error('err', 'fetchTableDataAndStatistData', err);
       });
   };
 
   const processData = (resArr) => {
+    const electorVotes = resArr[0];
+    const allNodeInfo = (resArr[2] ? resArr[2].value : [])
+      .sort((a, b) => +b.obtainedVotesAmount - +a.obtainedVotesAmount)
+      .map((item, index) => {
+        item.rank = index + 1;
+        return item;
+      });
+    let allTeamInfo = null;
+    const withdrawableVoteRecords = [];
+    let withdrawableVoteAmount = 0;
+    if (resArr[1].code === 0) {
+      allTeamInfo = resArr[1].data;
+    }
 
-    // 更新状态
+    const myVoteRecords = [
+      ...electorVotes.activeVotingRecords,
+      ...electorVotes.withdrawnVotesRecords,
+    ];
+    electorVotes.activeVotingRecords.forEach((record) => {
+      if (record.unlockTimestamp.seconds < moment().unix()) {
+        withdrawableVoteRecords.push(record);
+      }
+    });
+
+    // assign rank
+    myVoteRecords.forEach((record) => {
+      const foundedNode = allNodeInfo.find(
+        (item) => item.candidateInformation.pubkey === record.candidate,
+      );
+      if (foundedNode === undefined) {
+        // rank: used to sort
+        record.rank = 9999999;
+        // displayedRank: used to display
+        record.displayedRank = RANK_NOT_EXISTED_SYMBOL;
+      } else {
+        record.rank = foundedNode.rank;
+        record.displayedRank = foundedNode.rank;
+      }
+    });
+    const myTotalVotesAmount = electorVotes.allVotedVotesAmount;
+    withdrawableVoteAmount = withdrawableVoteRecords.reduce(
+      (total, current) => total + +current.amount,
+      0,
+    );
+    console.log({
+      myTotalVotesAmount,
+      withdrawableVoteAmount,
+    });
+    processStatistData(
+      'myTotalVotesAmount',
+      'num',
+      myTotalVotesAmount / ELF_DECIMAL,
+    );
+    processStatistData(
+      'withdrawableVotesAmount',
+      'num',
+      withdrawableVoteAmount / ELF_DECIMAL,
+    );
+    processTableData(myVoteRecords, allTeamInfo);
+  };
+
+  const processTableData = (myVoteRecords, allTeamInfo) => {
+    // add node name
+    const tableData = myVoteRecords;
+    tableData.forEach((record) => {
+      const teamInfo = allTeamInfo.find(
+        (team) => team.public_key === record.candidate,
+      );
+      console.log('teamInfo', teamInfo);
+      if (teamInfo === undefined) {
+        record.address = publicKeyToAddress(record.candidate);
+        record.name = addressFormat(record.address);
+      } else {
+        record.name = teamInfo.name;
+      }
+      if (record.isWithdrawn) {
+        record.type = 'Redeem';
+        record.operationTime = moment
+          .unix(record.withdrawTimestamp.seconds)
+          .format('YYYY-MM-DD HH:mm:ss');
+      } else if (record.isChangeTarget) {
+        record.type = 'Switch Vote';
+        record.operationTime = moment
+          .unix(record.voteTimestamp.seconds)
+          .format('YYYY-MM-DD HH:mm:ss');
+      } else {
+        record.type = 'Vote';
+        record.operationTime = moment
+          .unix(record.voteTimestamp.seconds)
+          .format('YYYY-MM-DD HH:mm:ss');
+      }
+      record.status = 'Success';
+      console.log('record.lockTime', record.lockTime);
+      const start = moment.unix(record.voteTimestamp.seconds);
+      const end = moment.unix(record.unlockTimestamp.seconds);
+      record.formattedLockTime = end.from(start, true);
+      record.formattedUnlockTime = end.format('YYYY-MM-DD HH:mm:ss');
+      record.isRedeemable = record.unlockTimestamp.seconds <= moment().unix();
+    });
+  
     setTableData(tableData);
     setSpinningLoading(false);
   };
+  
 
   const processStatistData = (key, param, value) => {
-    // 更新状态
     setStatistData({
       ...statistData,
       [key]: {
         ...(statistData[key] || {}),
-        [param]: value,
-      },
+        [param]: value
+      }
     });
     setSpinningLoading(false);
   };
+
 
   const onLogin = () => {
     getCurrentWallet();
