1ef9f15791c8ab26929d3be7e6fae8e529ef8525
Peterbjx <jianxiong.bai@aelf.io>
2023-08-09 15:47:40 +0800

    feat: change text color

--- src/_components/HeaderTitle/index.tsx
+++ src/_components/HeaderTitle/index.tsx
@@ -1,7 +1,7 @@
 export default function HeadTitle({ content }: { content: string }) {
   return (
     <div className="py-5">
-      <div className="text-xl font-bold not-italic decoration-base-100">{content}</div>
+      <div className="text-xl font-bold not-italic text-base-100">{content}</div>
     </div>
   );
 }
