9f8e0e5080ef147c817a2f729a3f7e524395830e
AbigailDeng <Abigail.deng@ienyan.com>
2023-12-01 14:37:36 +0800

    feat: modal

--- src/pages/Vote/ElectionNotification/CandidateApplyModal/CandidateApplyModal.js
+++ src/pages/Vote/ElectionNotification/CandidateApplyModal/CandidateApplyModal.js
@@ -153,6 +153,7 @@ class CandidateApplyModal extends PureComponent {
       <Modal
         className="apply-node-modal"
         destroyOnClose
+        getContainer={document.querySelector("#portkey-ui-root")}
         title={`Apply to Become a Block Porducer (BP) ${
           NETWORK_TYPE === "MAIN" ? " " : "on the Testnet"
         } `}
