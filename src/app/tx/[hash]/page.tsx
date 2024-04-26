ea929642cb87b33104c57dcd1afd31eed5248a03
Peterbjx <jianxiong.bai@aelf.io>
2024-04-26 17:47:09 +0800

    feat: chain

---
+++ src/app/tx/[hash]/page.tsx
@@ -0,0 +1,7 @@
+import { HashParams } from 'global';
+import Detail from './_components/detail';
+import fetchData from './mock';
+export default async function TransactionDetails({ params }: { params: HashParams }) {
+  const data = await fetchData({ transactionHash: params.hash });
+  return <Detail SSRData={data} />;
+}
